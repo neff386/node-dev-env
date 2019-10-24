64c3a9863a90857819c2609c5f76d6cedc208dd6
neff386 <day900@aol.com>
2019-10-24 14:25:33 -0400

    add users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,13 @@ app.get('/', (req,res) => {
  res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
+app.get('/users', (req,res) => {
+  res.json([
+            {"Name": "David"},
+            {"Name": "Tammy"}
+          ]);
+ })
+
 app.listen(port, (err) => {
   if (err){
     console.log(err);
