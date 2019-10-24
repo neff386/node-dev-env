9e2f71467d5ae76a6d4958507259a4a7ec36183b
neff386 <day900@aol.com>
2019-10-24 15:21:02 -0400

    added  delete user action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // populate Table of users via API call.
 getUsers().then(result => {
@@ -7,12 +7,27 @@ getUsers().then(result => {
   result.forEach(user => {
     usersBody+= `<tr>
     <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-    <td>${user.Name}</td>
+    <td.${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
     </tr>`
     
   });
 
 
 global.document.getElementById('users').innerHTML = usersBody;
+
+const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+Array.from(deleteLinks, link => {
+  link.onclick = function(event) {
+    const element = event.target;
+    event.preventDefault();
+    deleteUser(element.attributes["data-id"].value);
+    const row = element.parentNode.parentNode;
+    row.parentNode.removeChilde(row);
+  }
+})
 
 });
