cd48ba2614929f98e4db4caf33e46794201b378c
neff386 <day900@aol.com>
2019-10-24 20:20:42 -0400

    update to prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -14,11 +16,15 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Generate an external css file with a has 
+    new ExtractTextPlugin('[name].[contenthash].css'),
 
-    new webpack.optimize.ComminsChunkPlugin({
+    new WebpackMd5Hash(),
+
+    new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     }),
     //create HTML file that includes references to bundle JS.
@@ -36,7 +42,8 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject: true
+      inject: true,
+      trackJSToken: `a6036bfe4fe046819a9dab1a0fad23c8`
     }),
 
     //Eliminate duplicate packages when generating bundle
@@ -47,7 +54,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style', 'css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract(`css?sourceMap`)}
     ]
   }
 }
